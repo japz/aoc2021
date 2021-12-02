2a691729b90811a20561489e15fdba381acc3f3e
Jasper Capel <jcapel@ebay.com>
2021-12-02 08:18:41 +0100

    day 2

---
+++ d2/part1.py
@@ -0,0 +1,36 @@
+class Submarine(object):
+    pos_x: int
+    pos_z: int
+
+    def __init__(self):
+        self.pos_x = 0
+        self.pos_z = 0
+
+    def forward(self, n: int):
+        self.pos_x += n
+
+    def down(self, n: int):
+        self.pos_z += n
+
+    def up(self, n: int):
+        self.pos_z -= n
+
+    def command(self, command: str):
+        match command.split():
+            case [action, n]:
+                getattr(self, action)(int(n))
+
+    @property
+    def product(self):
+        return self.pos_x * self.pos_z
+
+
+with open("input.txt") as f:
+    lines = f.readlines()
+
+sub = Submarine()
+
+for command in lines:
+    sub.command(command)
+
+print(sub.product)
---
+++ d2/part2.py
@@ -0,0 +1,39 @@
+class Submarine(object):
+    aim: int
+    pos_x: int
+    pos_z: int
+
+    def __init__(self):
+        self.aim = 0
+        self.pos_x = 0
+        self.pos_z = 0
+
+    def forward(self, n: int):
+        self.pos_x += n
+        self.pos_z += n * self.aim
+
+    def down(self, n: int):
+        self.aim += n
+
+    def up(self, n: int):
+        self.aim -= n
+
+    def command(self, command: str):
+        match command.split():
+            case [action, n]:
+                getattr(self, action)(int(n))
+
+    @property
+    def product(self):
+        return self.pos_x * self.pos_z
+
+
+with open("input.txt") as f:
+    lines = f.readlines()
+
+sub = Submarine()
+
+for command in lines:
+    sub.command(command)
+
+print(sub.product)
